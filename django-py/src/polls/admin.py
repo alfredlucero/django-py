2ae03b8ab73f8923eb8c33232c626e1dc37e5dcc
Alfred Lucero <alfred.lucero@ucla.edu>
2022-09-05 20:49:55 -0700

    Customized admin interfaces more

--- django-py/src/polls/admin.py
+++ django-py/src/polls/admin.py
@@ -2,7 +2,24 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+# Customize the admin experience by creating a model admin class, then pass it in as second argument to register
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+  # fields = ['pub_date', 'question_text']
+  fieldsets = [
+    (None, { 'fields': ['question_text'] }),
+    ('Date information', { 'fields': ['pub_date'] })
+  ]
+  # Choice objects are edited on the Question admin page, provide enough fields for 3 choices
+  inlines = [ChoiceInline]
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
+  list_filter = ['pub_date']
+  search_fields = ['question_text']
+
 # Register your models here.
 # For polls app to display on the admin index page, we need to register the models
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
--- django-py/src/polls/models.py
+++ django-py/src/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 # Model = single, definitive source of information about your data
@@ -22,6 +23,11 @@ class Question(models.Model):
   pub_date = models.DateTimeField('date published')
   def __str__(self):
     return self.question_text
+  @admin.display(
+    boolean=True,
+    ordering='pub_date',
+    description='Published recently?',
+  )
   def was_published_recently(self):
     now = timezone.now()
     return now - datetime.timedelta(days=1) <= self.pub_date <= now
