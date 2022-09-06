003033be9c46705febf154bf9324fdfbf9eefead
Alfred Lucero <alfred.lucero@ucla.edu>
2022-09-05 20:24:54 -0700

    Added details tests

--- django-py/src/polls/tests.py
+++ django-py/src/polls/tests.py
@@ -6,6 +6,10 @@ from django.urls import reverse
 
 from .models import Question
 
+# Great rule of thumb to have a separate TestClass for each model or view
+# Separate test method for each set of conditions you want to test
+# Test method names that describe their function
+
 def create_question(question_text, days):
   """
   Create a question with the given question_text and published the given number of days offset to now (negative for questions published in the past, positive for questions that have yet to be published).
@@ -57,6 +61,25 @@ class QuestionIndexViewTests(TestCase):
     question2 = create_question(question_text="Past question 2.", days=-5)
     response = self.client.get(reverse('polls:index'))
     self.assertQuerysetEqual(response.context['latest_question_list'], [question2, question1])
+
+class QuestionDetailViewTests(TestCase):
+  def test_future_question(self):
+    """
+    The detail view of a question with a pub_date in the future returns a 404 not found.
+    """
+    future_question = create_question(question_text='Future question.', days=5)
+    url = reverse('polls:detail', args=(future_question.id,))
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 404)
+
+  def test_past_question(self):
+    """
+    The detail view of a question with a pub_date in the past displays the question's text.
+    """
+    past_question = create_question(question_text='Past Question.', days=-5)
+    url = reverse('polls:detail', args=(past_question.id,))
+    response = self.client.get(url)
+    self.assertContains(response, past_question.question_text)
 
 # Create your tests here.
 class QuestionModelTests(TestCase):
