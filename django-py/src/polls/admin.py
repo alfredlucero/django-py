51b49b190316a8cd8d11fce115d5f386e0a23968
Alfred Lucero <alfred.lucero@ucla.edu>
2022-09-05 20:31:12 -0700

    Added static stylesheet

--- django-py/src/polls/admin.py
+++ django-py/src/polls/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 # For polls app to display on the admin index page, we need to register the models
 admin.site.register(Question)
+admin.site.register(Choice)
